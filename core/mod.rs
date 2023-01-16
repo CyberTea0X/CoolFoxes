0d801d5e7e7d5c7c4c97ddb82ad5a7078692b103
CyberTea0X <mihailusov_778@mail.ru>
2023-01-16 21:31:28 +0300

    Удалил core.rs (Всё что связано с движком лежит в модуле engine) Изменил engine(lib.rs) - Добавил немного документации Переместил сцены ближе к корню игры

--- core/mod.rs
+++
@@ -1,9 +0,0 @@
-//! # CORE
-//!
-//! `core` это движок игры, в нём содержаться различные модули игры, такие как
-//! `sounds`, `physics`, `graphics`, а также некоторые полезные функции
-
-
-mod sounds;
-mod physics;
-mod graphics;
--- game/engine/src/lib.rs
+++ game/engine/src/lib.rs
@@ -1,3 +1,7 @@
+//! # ENGINE
+//!
+//! `engine` это движок игры, в нём содержаться различные модули игры, такие как
+//! `sounds`, `physics`, `graphics`, а также некоторые полезные функции
 pub mod graphics;
 pub mod physics;
 pub mod sounds;
